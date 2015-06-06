4debb761c121d72cb49b6053537f84d06d1c054d
MaVic14 <vasquez.mv@hotmail.com>
2015-06-06 03:28:23 -0300

    Fix 2
    Retocar la parte de usuarios porque no funcionaba. Agrego tmb la parte
    que Emiliando dejo para deshabilitar los botones.

--- segunda.php
+++ segunda.php
@@ -1,5 +1,4 @@
-
-<?php include("includes/top_page.php"); ?>
+﻿<?php include("includes/top_page.php"); ?>
 <?php include("includes/header.php"); ?>
 <div id="contenido">
 <?php 
@@ -13,14 +12,11 @@ if (!isset($headers['Authorization'])){
     exit;
 }
 define("NROLEGAJO_ADMIN",1000);
-
 $auth = $headers['Authorization'];
-
 if (substr($auth,0,5) == 'NTLM ') {
     $msg = base64_decode(substr($auth, 5));
     if (substr($msg, 0, 8) != "NTLMSSP\x00")
         die('error header not recognised');
-
     if ($msg[8] == "\x01") {
         $msg2 = "NTLMSSP\x00\x02\x00\x00\x00".
             "\x00\x00\x00\x00". // target name len/alloc
@@ -29,7 +25,6 @@ if (substr($auth,0,5) == 'NTLM ') {
             "\x00\x00\x00\x00\x00\x00\x00\x00". // challenge
             "\x00\x00\x00\x00\x00\x00\x00\x00". // context
             "\x00\x00\x00\x00\x00\x00\x00\x00"; // target info len/alloc/offset
-
         header('HTTP/1.1 401 Unauthorized');
         header('WWW-Authenticate: NTLM '.trim(base64_encode($msg2)));
         exit;
@@ -57,8 +52,8 @@ $numeroDia = date("N");
 $letraDia = date ("l");
 $nroLegajo = null;
 //Datos del insert
-$queryAsistencia = null;
 
+$queryAsistencia = null;
 $hs = null;
 $result = mysql_query("SELECT CURTIME()");
  
@@ -66,26 +61,20 @@ while($row=mysql_fetch_array($result))
 {
 	$hs = ($row["CURTIME()"]);
 }
+//Ahora van a poner el nombre del usuario el mismo que USUARIOCORTO que esta en la base y con mayuscula
+$resultEmpleados = mysql_query("SELECT * FROM EMPLEADOS where USUARIOCORTO = '".$user."'", $link);
 
-
-$usuarioCorto = explode('@', $user);
-$usuarioCorto_ = ltrim($usuarioCorto[0]) + '';//Agregue estas comillas porque no me tomaba el String
-
-$resultEmpleados = mysql_query("SELECT * FROM EMPLEADOS where USUARIOCORTO = $usuarioCorto_", $link);
-$row = mysql_fetch_row($resultEmpleados);
-
-if($resultEmpleados == null){
-
+if($resultEmpleados === FALSE){
 	echo "<div align=\"center\"> LOS DATOS INGRESADOS SON INCORRECTOS.</div><br>";
 	return;
 	
 } else {
-
+	$row = mysql_fetch_row($resultEmpleados);
+	
 	echo "<h3 align=\"center\" style=\"color:gray\"> Bienvenido ".ucfirst($row[1]).". Tu PC esta alojada como $domain/$workstation"."</h3><br>";
 	$result = mysql_query("SELECT * FROM comidas where numeroDia = $numeroDia", $link);
 	
 	$hs = $hs + '';
-
 	if($hs > 11){
 		echo "<h4 style=\"color:white;background-color:rgb(128,128,128)\">El horario para darse de ALTA/BAJA ha pasado. Cualquier duda contactese con Sergio.</h4>";
 		
@@ -98,16 +87,20 @@ if($resultEmpleados == null){
 	echo "<h4 style=\"color:gray\">Guarnición: ".mysql_result($result, 0, "guarnicion")."</h4>";
 	
 	$nroLegajo = $row[2];
-	
+
 	if($nroLegajo == NROLEGAJO_ADMIN){
 		$result = mysql_query("SELECT COUNT(*) FROM asistencia where numeroDia = $numeroDia", $link);
 		echo "<h4 style=\"color:gray\">Cantidad de comensales</em></u>: ".mysql_result($result, 0)."</h4>";
 	}
 	if($hs > 11){
 		return;
-	}	
+	}
+
 	//EMI, aca esta lo de no permitir votar 2 veces. Hace la consulta preguntando si el legajo existe
 	$queryAsistencia = mysql_query("SELECT * FROM asistencia where LEGAJO = $nroLegajo and NUMERODIA = $numeroDia");
+	if($queryAsistencia != null){
+		//echo "<h4 style=\"color:white;background-color:rgb(128,128,128)\">Recuerde que usted ya se ha dado de ALTA para asistir, lo esperamos!.</h4>";
+	}
 }
 ?>
 
@@ -130,31 +123,37 @@ if($resultEmpleados == null){
 	<br>
 </form>
 <style type="text/css"> 
-
 body{
-
 background:#aaa;
-
 }
-
 </style> 
 
 <?php
-if (isset($_POST["alta"])) {
-	 
+if (isset($_POST["alta"])) {?> 
+	<script>
+	 $('input[name="alta"]').attr('disabled','disabled')
+	 </script><?php
 		$sql = "INSERT INTO asistencia (numeroDia, letraDia,usuarioNombre, legajo)";
 		$sql.= "VALUES ('".$numeroDia."', '".$letraDia."', '".$user."', '".$nroLegajo."')";
 		mysql_query($sql);
+		if(mysql_error()){
+			//echo "Le comunicamos que usted ya se ha dado de ALTA.";
+			return;
+		}
 		$status = "ok";
-
 }	
 if (isset($_POST["baja"])) {
+?> 
+	<script>
+	 $('input[name="baja"]').attr('disabled','disabled')
+	 </script><?php
         $sql = "DELETE FROM asistencia ";
         $sql.= "WHERE legajo = ('".$nroLegajo."')";
         mysql_query($sql);
         $status = "ok";
 		}
-?> 
+?>
+
 <?php include("includes/footer.php"); ?>
 <script>
 function abrirPopUpAlta()
